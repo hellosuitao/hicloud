89ec0e4995f758d5f646ba93ef30ed3d95f3ad44
horeizn <1174252664@qq.com>
2019-10-31 12:03:49 +0800

    Figen-》figen

--- service-order-feign/src/main/java/com/ruixun/serviceorderfeign/controller/FeignController.java
+++ service-order-feign/src/main/java/com/ruixun/serviceorderfeign/controller/FeignController.java
@@ -2,14 +2,12 @@ package com.ruixun.serviceorderfeign.controller;
 
 import com.ruixun.serviceorderfeign.service.MemberFeign;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@ResponseBody
-@Controller
+@RestController
 @RequestMapping("/order")
 public class FeignController {
 
